c0a6aa28eda85438c56643f4db09faa32725dd4a
kiisaaa <kenpascualjacob@gmail.com>
2025-07-23 00:51:30 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = 'pomodoro-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
